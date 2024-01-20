174c88852bc2079d5b3d7ad858b8bea67b4aa714
Mathew Seng <mathewseng@Mathews-MacBook-Pro.local>
2024-01-19 23:50:31 -0600

    ENH: Angular new

--- client/plnts_2/src/app/app.routes.ts
+++ client/plnts_2/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { PlantListComponent } from './components/plant-list/plant-list.component';
-import { AddPlantComponent } from './components/add-plant/add-plant.component';
 
-export const appRoutes: Routes = [
-  { path: '', component: PlantListComponent },
-  { path: 'add-plant', component: AddPlantComponent },
-  { path: '**', redirectTo: '' },
-];
+export const routes: Routes = [];
