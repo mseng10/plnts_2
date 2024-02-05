7413db72be71c9c0b13258d8f0593e2b3d21c62f
mseng10 <mathewseng@gmail.com>
2024-02-04 21:33:54 -0600

    ENH: Move to update button

--- client/react/src/Plant.js
+++ client/react/src/Plant.js
@@ -1,26 +1,32 @@
 // Plant.js
 import React from 'react';
 
-const Plant = ({ id, name, type, wateringFrequency, alive, lastWatered, onClick, onKill, onWater }) => {
+const Plant = ({ name, type, wateringFrequency, lastWatered, alive, onKill, onWater, onUpdate }) => {
   const handleWaterClick = () => {
-    // Call the onWater function with the plant's id
-    onWater(id);
+    // Call the onWater function
+    onWater();
+  };
+
+  const handleUpdateClick = () => {
+    // Call the onUpdate function
+    onUpdate();
   };
 
   return (
-    <div className={`plant ${alive ? 'alive' : 'not-alive'}`} onClick={onClick}>
-      <h3>
-        {name}
-        {alive && (
-          <>
-            <button onClick={() => onKill(id)}>Kill</button>
+    <div className={`plant ${alive ? 'alive' : 'not-alive'}`}>
+      <h3>{name}</h3>
+      {alive && (
+        <>
+          <p>Type: {type}</p>
+          <p>Watering Frequency: {wateringFrequency}</p>
+          {lastWatered && <p>Last Watered: {lastWatered}</p>}
+          <div>
             <button onClick={handleWaterClick}>Water</button>
-          </>
-        )}
-      </h3>
-      <p>Type: {type}</p>
-      <p>Watering Frequency: {wateringFrequency}</p>
-      {alive && <p>Last Watered: {lastWatered}</p>}
+            <button onClick={onKill}>Kill</button>
+            <button onClick={handleUpdateClick}>Update</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
