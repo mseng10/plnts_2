7db5639a7115390cc2e51fa90eb8488ab2bceb70
mseng10 <mathewseng@gmail.com>
2024-04-20 11:25:17 -0500

    MISC: Remove config

--- server/models/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    """Configuration for this process."""
-
-    def __init__(self, test_mode: bool) -> None:
-        self.test_mode: bool = test_mode
-
-    def __repr__(self):
-        return "Configuration:\n" + f" - Test Mode: {self.test_mode}"
