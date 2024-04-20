1c605e912e7c3436e591c5f0087b3080ee9d9a73
mseng10 <mathewseng@gmail.com>
2024-04-20 11:25:51 -0500

    ENH: Create type, genus and species models for plant

--- server/models/plant.py
+++ server/models/plant.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from enum import Enum
 
-class MyEnum(Enum, str):
+
+class DeathCause(Enum, str):
     TOO_LITTLE_WATER = 'too little water'
     TOO_MUCH_WATER = 'too much water'
     TOO_LITTLE_HUMIDITY = 'too little humidity'
@@ -15,38 +16,73 @@ class MyEnum(Enum, str):
     PROPAGATION = 'propagation'
     PESTS = 'pests'
     MOLD = 'mold'
+    NEGLECT = 'neglect'
     UNKNOWN = 'unknown'
+
 
 class Plant(Base):
     """Plant model."""
+    __tablename__ = "plant"
 
-    __tablename__ = "plants"
-
+    # Created at specs
     id = Column(Integer(), primary_key=True)
+    created_on = Column(DateTime(), default=datetime.now)
+    cost = Column(Integer())
+    size = Column(Integer())  # inches
+    type_id: Mapped[int] = mapped_column(ForeignKey("type.id")) # Type of Plant
+
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
 
     # Batch
     batch_id: Mapped[int] = mapped_column(ForeignKey("batch.id"))
     batch: Mapped["Batch"] = relationship(back_populates="plants")
 
-    # Created at specs
-    created_on = Column(DateTime(), default=datetime.now)
-    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
-    cost = Column(Integer())
-
-    size = Column(Integer()) # inches
-    name = Column(String(100), nullable=False)
-    genus = Column(String(100), nullable=False)  # should be shared
-    type = Column(String(100), nullable=False)  # should be shared
-
     # Water Info
     watered_on = Column(DateTime(), default=datetime.now)
-    watering = Column(Integer()) # days
+    watering = Column(Integer())  # days
 
     # Death Info
-    dead_on = Column(DateTime(), default=None, nullable=True)
     dead = Column(Boolean, default=False, nullable=False)
-    cause = Column(String(100), nullable=False)
-    value = Column(Enum(MyEnum))
+    dead_cause = Column(Enum(DeathCause), nullable=True)
+    dead_on = Column(DateTime(), default=None, nullable=True)
 
     def __repr__(self) -> str:
         return f"{self.name} ({self.type}/{self.genus})"
+
+class Type(Base):
+    """Type of genus."""
+    __tablename__ = "type"
+
+    id = Column(Integer(), primary_key=True)
+    created_on = Column(DateTime(), default=datetime.now)
+    name = Column(String(100), nullable=False)
+
+    plants: Mapped[List["Plant"]] = relationship()
+
+    # TODO: Best lighting and soil mixes
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+class Genus(Base):
+    """Genus of species."""
+    __tablename__ = "genus"
+
+    id = Column(Integer(), primary_key=True)
+    created_on = Column(DateTime(), default=datetime.now)
+    name = Column(String(100), nullable=False, unique=True)
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+
+class Species(Base):
+    """Species of plants."""
+    __tablename__ = "species"
+
+    id = Column(Integer(), primary_key=True)
+    created_on = Column(DateTime(), default=datetime.now)
+    name = Column(String(100), nullable=False, unique=True)
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
