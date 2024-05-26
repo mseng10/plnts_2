6f6ffb9dcf2929d96343f18e0ba3a83b23b222c8
mseng10 <mathewseng@gmail.com>
2024-05-26 17:20:41 -0500

    ENH: Species endpoints

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import URL
 import json
 import logging
-from models.plant import Plant, Base  # Importing the Plant model
+from models.plant import Plant, Base, Species, Genus  # Importing the Plant model
 
 # Initialize Colorama
 # init(autoreset=True)
@@ -41,13 +41,13 @@ logger = logging.getLogger(__name__)
 
 @app.route("/plants", methods=["POST"])
 def add_plant():
+    logger.info("Attemping create plant")
     # Get JSON data from request
     new_plant_data = request.get_json()
     # Create a new Plant object
     new_plant = Plant(
         cost=new_plant_data["cost"],
-        size=new_plant_data["size"],
-        watering=new_plant_data["watering"],
+        size=new_plant_data["size"]
     )
     # Add the new plant object to the session
     session = Session()
@@ -69,6 +69,55 @@ def get_plants():
     plants_json = [{"id": plant.id, "cost": plant.cost, "size": plant.size, "watering": plant.watering} for plant in plants]
     # Return JSON response
     return jsonify(plants_json)
+
+@app.route("/species", methods=["POST"])
+def add_species():
+    logger.info("Attempting create species")
+
+    new_species_data = request.get_json()
+
+    genus_id = new_species_data["genus_id"]
+
+    # I'm electing not to have a dedicated endpoint to creating genuses... sue me
+    # We can just embed it on the species object for now
+    new_genus = None
+    if not genus_id:
+        if not new_species_data["genus"]:
+            return jsonify({"message": "Did not specify genus when creating species"}), 400
+        
+        new_genus_data = new_species_data["genus"]
+        new_genus = Genus(
+            name=new_genus_data["name"],
+            watering=new_genus_data["watering"]
+        )
+    
+    # Create a new Type object
+    new_species = Species(
+        name=new_species_data["name"],
+        genus_id=genus_id
+    )
+
+    # Add the new species (and genus if applicable) object to the session
+    session = Session()
+    if new_genus:
+        session.add(new_genus)
+    session.add(new_type)
+    session.commit()
+    session.close()
+
+    return jsonify({"message": "Species added successfully", "id": new_species.id}), 201
+
+@app.route("/species", methods=["GET"])
+def get_species():
+    logger.info("Received request to retrieve all plant species")
+
+    session = Session()
+    species = session.query(Species).all()
+    session.close()
+    # Transform species to JSON format
+    species_json = [{"id": _species.id, "name": _species.name} for _species in species]
+    # Return JSON response
+    return jsonify(species_json)
 
 if __name__ == "__main__":
     # Run the Flask app
