bffef0b4dff61e4072d611e2128ea5e8d074c082
mseng10 <mathewseng@gmail.com>
2024-05-26 23:10:33 -0500

    ENH: Genus - Species - Plant mappings

--- server/models/plant.py
+++ server/models/plant.py
@@ -9,17 +9,17 @@ from enum import Enum
 Base = declarative_base()
 
 # class DeathCause(Enum, str):
-    # TOO_LITTLE_WATER = "too little water"
-    # TOO_MUCH_WATER = "too much water"
-    # TOO_LITTLE_HUMIDITY = "too little humidity"
-    # TOO_MUCH_HUMIDITY = "too much humidity"
-    # TOO_LITTLE_SUN = "too little sun"
-    # TOO_MUCH_SUN = "too much sun"
-    # PROPAGATION = "propagation"
-    # PESTS = "pests"
-    # MOLD = "mold"
-    # NEGLECT = "neglect"
-    # UNKNOWN = "unknown"
+#     TOO_LITTLE_WATER = "too little water"
+#     TOO_MUCH_WATER = "too much water"
+#     TOO_LITTLE_HUMIDITY = "too little humidity"
+#     TOO_MUCH_HUMIDITY = "too much humidity"
+#     TOO_LITTLE_SUN = "too little sun"
+#     TOO_MUCH_SUN = "too much sun"
+#     PROPAGATION = "propagation"
+#     PESTS = "pests"
+#     MOLD = "mold"
+#     NEGLECT = "neglect"
+#     UNKNOWN = "unknown"
 
 
 class Plant(Base):
@@ -30,67 +30,50 @@ class Plant(Base):
     # Created at specs
     id = Column(Integer(), primary_key=True)
     created_on = Column(DateTime(), default=datetime.now)
-    cost = Column(Integer())
-    size = Column(Integer())  # inches
-    type_id: Mapped[int] = mapped_column(ForeignKey("type.id"))  # Type of Plant
+    cost = Column(Integer(), default=0, nullable=False)
+    size = Column(Integer(), default=0, nullable=False)  # inches
+    species_id: Mapped[int] = mapped_column(ForeignKey("species.id", ondelete='CASCADE'))  # Species of Plant
 
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
 
-    # Batch
-    batch_id: Mapped[int] = mapped_column(ForeignKey("batch.id"))
-    batch: Mapped["Batch"] = relationship(back_populates="plants")
-
     # Water Info
     watered_on = Column(DateTime(), default=datetime.now)
-    watering = Column(Integer())  # days
 
     # Death Info
     dead = Column(Boolean, default=False, nullable=False)
     # dead_cause = Column(Enum(DeathCause), nullable=True)
     dead_on = Column(DateTime(), default=None, nullable=True)
 
-    def __repr__(self) -> str:
-        return f"{self.name} ({self.type}/{self.genus})"
-
-
-class Type(Base):
-    """Type of genus."""
-
-    __tablename__ = "type"
-
-    id = Column(Integer(), primary_key=True)
-    created_on = Column(DateTime(), default=datetime.now)
-    name = Column(String(100), nullable=False)
-
-    plants: Mapped[List["Plant"]] = relationship()
-
-    # TODO: Best lighting and soil mixes
-
-    def __repr__(self) -> str:
-        return f"{self.name}"
-
-
-class Genus(Base):
-    """Genus of species."""
-
-    __tablename__ = "genus"
-
-    id = Column(Integer(), primary_key=True)
-    created_on = Column(DateTime(), default=datetime.now)
-    name = Column(String(100), nullable=False, unique=True)
-
-    def __repr__(self) -> str:
-        return f"{self.name}"
-
-
 class Species(Base):
-    """Species of plants."""
+    """Species of genus."""
 
     __tablename__ = "species"
 
     id = Column(Integer(), primary_key=True)
     created_on = Column(DateTime(), default=datetime.now)
     name = Column(String(100), nullable=False, unique=True)
+
+    # Available plants of this species
+    plants: Mapped[List["Plant"]] = relationship('Plant', backref='plant', passive_deletes=True)
+
+    # Genus of this species of plant
+    genus_id: Mapped[int] = mapped_column(ForeignKey("genus.id", ondelete='CASCADE'))
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+class Genus(Base):
+    """Genus of plant."""
+
+    __tablename__ = "genus"
+
+    id = Column(Integer(), primary_key=True)
+    created_on = Column(DateTime(), default=datetime.now)
+    name = Column(String(100), nullable=False, unique=True)
+    watering = Column(Integer())  # days
+
+    # Available species of this genus
+    species: Mapped[List["Species"]] = relationship('Species', backref='species', passive_deletes=True)
 
     def __repr__(self) -> str:
         return f"{self.name}"
