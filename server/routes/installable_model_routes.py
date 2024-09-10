e87231d0a9f0afe5e122b87683e73bcb10b8debd
mseng10 <mathewseng@gmail.com>
2024-09-09 21:45:28 -0500

    MISC: install model routes abstraction

--- server/routes/installable_model_routes.py
+++ server/routes/installable_model_routes.py
@@ -1,17 +1,25 @@
 from flask import Blueprint, jsonify, request
 
 from routes import GenericCRUD, APIBuilder
-from models.plant import Genus, Type
+from models.plant import PlantGenusType, PlantGenus, PlantSpecies
 from models.mix import Soil
 
-types_bp = Blueprint('types', __name__)
-type_crud = GenericCRUD(Type, Type.schema)
-APIBuilder.register_resource(types_bp, 'types', type_crud, methods=["GET", "GET_MANY", "POST"])
+genus_types_bp = Blueprint('genus_types', __name__)
 
-genuses_bp = Blueprint('genuses', __name__)
-genus_crud = GenericCRUD(Genus, Genus.schema)
-APIBuilder.register_resource(genuses_bp, 'genuses', genus_crud, methods=["GET", "GET_MANY", "POST"])
+genus_types_crud = GenericCRUD(PlantGenusType, PlantGenusType.schema)
+genus_crud = GenericCRUD(PlantGenus, PlantGenus.schema)
+
+APIBuilder.register_resource(genus_types_bp, 'genus_types', genus_types_crud, methods=["GET", "GET_MANY"])
+APIBuilder.register_resource(genus_types_bp, 'genera', genus_crud, methods=["GET", "GET_MANY"])
+
+species_bp = Blueprint('species', __name__)
+species_crud = GenericCRUD(PlantSpecies, PlantSpecies.schema)
+APIBuilder.register_resource(species_bp, 'species', species_crud, methods=["GET", "GET_MANY"])
+
+genus_bp = Blueprint('genera', __name__)
+genus_crud = GenericCRUD(PlantGenus, PlantGenus.schema)
+APIBuilder.register_resource(genus_bp, 'genera', genus_crud, methods=["GET", "GET_MANY"])
 
 soils_bp = Blueprint('soils', __name__)
 soils_crud = GenericCRUD(Soil, Soil.schema)
-APIBuilder.register_resource(soils_bp, 'soils', soils_crud, methods=["GET", "GET_MANY", "POST"])
+APIBuilder.register_resource(soils_bp, 'soils', soils_crud, methods=["GET", "GET_MANY"])
